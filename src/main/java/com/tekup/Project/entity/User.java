3bf3e0a5addefb595700a13d1510005b9e7d504b
Oussema <oussama.chercheri@tek-up.de>
2023-11-20 20:14:13 +0100

    first commit

---
+++ src/main/java/com/tekup/Project/entity/User.java
@@ -0,0 +1,23 @@
+package com.tekup.Project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+    private String fullname;
+    private String email;
+    private String dob;
+    private String password;
+}
---
+++ src/main/java/com/tekup/Project/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.tekup.Project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tekup.Project.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer > {
+
+    User findByEmail(String email);
+}
